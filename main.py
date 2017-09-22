e6efe74ee4799345c2c9a0cafb4f55d6bd4dfd92
Jailson Pereira <jailsoncolares@gmail.com>
2017-09-22 16:15:24 -0400

    adicionando arquivos base do sistema

---
+++ main.py
@@ -0,0 +1,10 @@
+from db_users import avaliacoes
+
+ava =  avaliacoes ['Ana']['Star Trek']
+ava1 = avaliacoes ['Marcos']['Star Trek']
+
+print(ava)  #imprime a nota de avaliação do filme Star Trek , avaliado por Ana
+print(ava1) #imprime a nota de avaliação do filme Star Trek , avaliado por Marcos
+print('################')
+print ('Todos os filmes da ana com suas notas de avalição: ')
+print(avaliacoes['Ana']) #imprime todos os filmes avaliados por Ana
