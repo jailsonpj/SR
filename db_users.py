82c35925642723fc738080a8c83be1f034b2733a
jailsonpj <jailsoncolares@gmail.com>
2018-01-11 16:15:10 -0400

    feat:funcão de similaridade de usuarios

--- db_users.py
+++ db_users.py
@@ -4,50 +4,50 @@ from math import sqrt
 
 
 avaliacoes = {'Ana': #chave ana tem , chave filme e um valor nota para o determinado filme
-                {'Freddy x Jason': 2.5, 
+                {'Freddy x Jason': 2.5,
                     'O Ultimato Bourne': 3.5,
-                    'Star Trek': 3.0, 
-                    'Exterminador do Futuro': 3.5, 
-                    'Norbit': 2.5, 
-                    'Star Wars': 3.0},
-    
-            'Marcos': 
-                {'Freddy x Jason': 3.0, 
-                    'O Ultimato Bourne': 3.5, 
-                    'Star Trek': 1.5, 
-                    'Exterminador do Futuro': 5.0, 
-                    'Star Wars': 3.0, 
-                    'Norbit': 3.5}, 
-
-            'Pedro': 
-                {'Freddy x Jason': 2.5, 
-                    'O Ultimato Bourne': 3.0,
-                    'Exterminador do Futuro': 3.5, 
-                    'Star Wars': 4.0},
-            
-            'Claudia': 
-                {'O Ultimato Bourne': 3.5, 
                     'Star Trek': 3.0,
-                    'Star Wars': 4.5, 
-                    'Exterminador do Futuro': 4.0, 
-                    'Norbit': 2.5},
-                
-            'Adriano': 
-                {'Freddy x Jason': 3.0, 
-                    'O Ultimato Bourne': 4.0, 
-                    'Star Trek': 2.0, 
-                    'Exterminador do Futuro': 3.0, 
-                    'Star Wars': 3.0,
-                    'Norbit': 2.0}, 
+                    'Exterminador do Futuro': 3.5,
+                    'Norbit': 2.5,
+                    'Star Wars': 3.0},
 
-            'Janaina': 
-                {'Freddy x Jason': 3.0, 
-                    'O Ultimato Bourne': 4.0,
-                    'Star Wars': 3.0, 
-                    'Exterminador do Futuro': 5.0, 
+            'Marcos':
+                {'Freddy x Jason': 3.0,
+                    'O Ultimato Bourne': 3.5,
+                    'Star Trek': 1.5,
+                    'Exterminador do Futuro': 5.0,
+                    'Star Wars': 3.0,
                     'Norbit': 3.5},
-    
-            'Leonardo': 
+
+            'Pedro':
+                {'Freddy x Jason': 2.5,
+                    'O Ultimato Bourne': 3.0,
+                    'Exterminador do Futuro': 3.5,
+                    'Star Wars': 4.0},
+
+            'Claudia':
+                {'O Ultimato Bourne': 3.5,
+                    'Star Trek': 3.0,
+                    'Star Wars': 4.5,
+                    'Exterminador do Futuro': 4.0,
+                    'Norbit': 2.5},
+
+            'Adriano':
+                {'Freddy x Jason': 3.0,
+                    'O Ultimato Bourne': 4.0,
+                    'Star Trek': 2.0,
+                    'Exterminador do Futuro': 3.0,
+                    'Star Wars': 3.0,
+                    'Norbit': 2.0},
+
+            'Janaina':
+                {'Freddy x Jason': 3.0,
+                    'O Ultimato Bourne': 4.0,
+                    'Star Wars': 3.0,
+                    'Exterminador do Futuro': 5.0,
+                    'Norbit': 3.5},
+
+            'Leonardo':
                 {'O Ultimato Bourne':4.5,
                     'Norbit':1.0,
                     'Exterminador do Futuro':4.0}
@@ -65,8 +65,13 @@ def euclidiana(user1,user2):
         return 0
 
     soma = sum ([pow(avaliacoes[user1][item] - avaliacoes[user2][item],2)
-                    for item in avaliacoes[user1] if item in avaliacoes[user2] 
+                    for item in avaliacoes[user1] if item in avaliacoes[user2]
                 ])
     return 1/(1+sqrt(soma))
 
-
+def getSimilares(user): #retorna a similaridade de um usuários com todos os outros usuários
+    similaridade = [(euclidiana(user, outro), outro)
+                    for outro in avaliacoes if outro != user]
+    similaridade.sort()
+    similaridade.reverse()
+    return similaridade
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from db_users import avaliacoes,euclidiana
+from db_users import avaliacoes,euclidiana,getSimilares
 
 
 ava =  avaliacoes ['Ana']['Star Trek']
 ava1 = avaliacoes ['Marcos']['Star Trek']
 
-print(ava)  #imprime a nota de avaliação do filme Star Trek , avaliado por Ana
+'''print(ava)  #imprime a nota de avaliação do filme Star Trek , avaliado por Ana
 print(ava1) #imprime a nota de avaliação do filme Star Trek , avaliado por Marcos
 print('################')
 print ('Todos os filmes da ana com suas notas de avalição: ')
-print(avaliacoes['Ana']) #imprime todos os filmes avaliados por Ana
+print(avaliacoes['Ana']) #imprime todos os filmes avaliados por Ana '''
 
-print('##############')
+'''print('##############')
 print(euclidiana('Leonardo','Ana'))
-print(euclidiana('Claudia','Ana'))
+print(euclidiana('Claudia','Ana')) #mostra a similaridade entre os usuários
+print(euclidiana('Ana','Pedro'))'''
+
+print(getSimilares('Leonardo'))
